5bb30ec3e4c7f5e4f6751b205738d11a5a4d1ab1
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-04-01 23:10:09 -0500

    agregue  Login, Proyectos y Nueva cuenta componentes y los enrute

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react'
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Login from './components/auth/Login'
+import NuevaCuenta from './components/auth/NuevaCuenta'
+import Proyectos from './components/proyectos/Proyectos'
 
 function App() {
   return (
-  <h1>MErn</h1>
-  );
+
+    //1. aca definimos lo que se va a ver en todas las paginas
+    <Router>
+
+
+      {/* 2. aca se va a ver lo que se ve por fuera */}
+      <Routes>
+        {/* 2.1 definimos nuestras rutas  */}
+        <Route exact path= '/' element={<Login/>} />
+        <Route exact path= '/nueva-cuenta' element={<NuevaCuenta/>} />
+        <Route exact path= '/Proyectos' element={<Proyectos/>} />
+      </Routes>
+
+
+    </Router>
+);
 }
 
 export default App;
