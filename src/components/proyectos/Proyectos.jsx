fe8b988e89470cd85b03005b2f8eb15da3bbb233
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-04-02 11:35:05 -0500

    agregue el componente form tarea y lo importe a proyectos

--- src/components/proyectos/Proyectos.jsx
+++ src/components/proyectos/Proyectos.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import Sidebar from '../layout/Sidebar';
 import Barra from '../layout/Barra';
+import FormTarea from '../tareas/FormTarea';
 
 const Proyectos = () => {
     return ( 
         <div className='contenedor-app'>
-            <Barra/>
+            
             <Sidebar/>
             
             <div className="seccion-principal">
-                
+                <Barra/>
                 <main>
+                    <FormTarea/>
+                    
                     <div className="contenedor-tareas">
-
+                    
                     </div>
                 </main>
             </div>
