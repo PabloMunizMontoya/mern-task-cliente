11c12e67f93927ef7d54061f1dd6b8a6a775d30b
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-04-02 11:08:45 -0500

    agregue los componentes proyeco y listadoproyecto al layout

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import NuevoProyecto from '../proyectos/NuevoProyecto';
+import ListadoProyectos from '../proyectos/ListadoProyectos';
+import Proyecto from '../proyectos/Proyecto';
 
 const Sidebar = () => {
     return ( 
         <aside>
             <h1>MERN<span>Tasks</span></h1>
-
+            <NuevoProyecto/>
             <div className="proyectos">
                 <h2>Tus Proyectos</h2>
-                <NuevoProyecto/>
+                <ListadoProyectos/>
             </div>
         </aside>
     );
