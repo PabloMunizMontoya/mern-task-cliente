db73c26b4175bde969e61fa515745fa802ed7ece
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-04-10 17:18:30 -0500

    cargamos la explicacion del codigo como comentarios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+//3.importamos los componentes que queremos mostrar en el componente principal app.
 import Login from './components/auth/Login'
 import NuevaCuenta from './components/auth/NuevaCuenta'
 import Proyectos from './components/proyectos/Proyectos'
@@ -10,16 +12,22 @@ function App() {
   return (
 
     <ProyectoState>
-      {/* //1. aca definimos lo que se va a ver en todas las paginas */}
+
+      {/* //1. aca definimos lo que se va a ver en todas las paginas, esto es dentro del router */}
       <Router>
-        {/* 2. aca se va a ver lo que se ve por fuera */}
+
+        {/* 2. en routes se va a ver cada una de las paginas */}
         <Routes>
-          {/* 2.1 definimos nuestras rutas  */}
+
+          {/* 2.1 definimos nuestras rutas, que hacen referencia a los componentes  */}
           <Route exact path= '/' element={<Login/>} />
           <Route exact path= '/nueva-cuenta' element={<NuevaCuenta/>} />
           <Route exact path= '/Proyectos' element={<Proyectos/>} />
+
         </Routes>
+
       </Router>
+      
     </ProyectoState>
 
     
--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from 'react'
+
+//9 importamos esta propiedad desde react router dom para hacer funcionales los enlaces.
 import {Link} from 'react-router-dom'
 
 const Login = () => {
 
+    // 7 definimos el state para tomar los datos del usuario, el estate tiene como datos iniciales un objeto con propiedades y su valor vació.
     const [usuario, guardarUsuario] = useState({
         email: '',
         password: ''
     })
 
+
+    //7.1 extraemos del usuario sus valores con destructuring
     const { email, password } = usuario
 
+    //4.2 corremos la function onChange que toma los valores que el usuario pone dentro del formulario. para no modificar los valores originales primero se crea una copia con spread operator, y luego se le asigna a esa copia el valor de los datos tomados en el evento.  
     const onChange = (e) => {
         guardarUsuario({
             ...usuario,
@@ -17,6 +23,7 @@ const Login = () => {
         })
     }
 
+    //8.1 function que se dispara al usuario apretar el submit, se pone un preventDefault para que la pagina no se recargue al hacer click en el boton. 
     const onSubmit = e => {
         e.preventDefault()
     }
@@ -27,8 +34,10 @@ const Login = () => {
                 <h1>Iniciar Sesión</h1>
 
                 <form 
+                    /* 8 ponemos el onSubmit que escucha cuando el usuario hace click en el boton */
                     onSubmit={onSubmit}
                 >
+                    {/* 4. ingresamos la parte del form donde el usuario pone su e mail */}
                     <div className='campo-form'>
                         <label htmlFor="email">Email</label>
                         <input 
@@ -37,9 +46,12 @@ const Login = () => {
                             name='email'
                             placeholder='Tu Email'
                             value={email}
+                            /* 4.1 creamos un onChange que hace que se ejecute una function cada que halla un cambio */
                             onChange={onChange}
                         />
                     </div>
+
+                    {/* 5 ingresamos la parte del formulario donde el usuario pone su password */}
                     <div className='campo-form'>
                         <label htmlFor="password">Password</label>
                         <input 
@@ -51,12 +63,15 @@ const Login = () => {
                             onChange={onChange}
                         />
                     </div>
+
+                    {/* 6 agregamos un boton para el submit del form */}
                     <div className='campo-front'>
                         <input type="submit" className='btn btn-primario btn-block' 
                         value='Iniciar Sesión'/>
                     </div>
                 </form>
 
+                {/* 9.1 agregamos Link y le especificamos la ruta, esta parte es por si el usuario no tiene cuenta en la pagina aparece un link para crear una nueva cuenta */}
                 <Link to = {'/nueva-cuenta'} className='enlace-cuenta'>
                     Obtener Cuenta
                 </Link>
