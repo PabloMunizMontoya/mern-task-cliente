f133457a0b69229b913c713b1f4d3cc8813c2927
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-04-09 23:06:16 -0500

    agregamos el listado de proyectos al state inicial usando context

--- src/context/proyectos/proyectoState.js
+++ src/context/proyectos/proyectoState.js
@@ -6,6 +6,11 @@ import {FORMULARIO_PROYECTO} from '../../types'
 
 const ProyectoState = props => {
     const initialState = {
+        proyectos : [
+            {id : 1, nombre: 'Tienda Virtual'},
+            {id : 2, nombre: 'Intranet'},
+            {id : 3, nombre: 'Diseño de sitio web'}
+        ],
         formulario : false
     }
 
@@ -23,6 +28,7 @@ const ProyectoState = props => {
     return(
         <proyectoContext.Provider
             value={{
+                proyectos: state.proyectos,
                 formulario: state.formulario,
                 mostrarFormulario
             }}
