a8c720178156cc2be9aca49c7ce5c66979e3c04e
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-04-11 22:55:04 -0500

    si no hay ningun proyecto no renderizamos el formulario para agregar tareas

--- src/components/tareas/FormTarea.jsx
+++ src/components/tareas/FormTarea.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 // 27. en este componente estará el formulario para agregar nuevas tareas al proyecto en especifico.
 const FormTarea = () => {
+
+    //100. importamos el context y le decimos que function vaa usar
+    const proyectosContext = useContext(proyectoContext)
+
+    //100.1 extraemos del context lo que queremos usar en el componente, queremos ocultar el formulario de tareas si proyectos es null. 
+    const { proyecto } = proyectosContext
+
+    //100.2 como estamos tratando de acceder a un proyecto en el siguiente paso usando el array destructuring, si no hay ningún proyecto va a saltar un error, para prevenir esto: 
+    if(!proyecto) return null
+
+
     return ( 
         <div className='formulario'>
             <form>
